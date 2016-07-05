a7e6053e347d52eb3bdd538893679049b5a7d2f4
Shikhar Srivastava <shikharsrivastav@gmail.com>
2016-07-06 01:37:07 +0530

    Updates

--- WEB-INF/classes/ExecuteServlet.java
+++ WEB-INF/classes/ExecuteServlet.java
@@ -31,11 +31,20 @@ public class ExecuteServlet extends HttpServlet{
       try {
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
+
             System.out.println("In ExecuteServlet");
+            
             String mType = request.getParameter("mType");	
-            String dType = request.getParameter("dType");
-            String data = request.getParameter("data");
-            int count;
+            System.out.println("mType: "+mType);
+            String imgName = request.getParameter("imgName");
+            System.out.println("imgName: "+imgName);
+            String[] imgNames=imgName.split('+');
+            System.out.println("Image(s) selected: ");
+            for(String img: imgNames)
+            {
+              System.out.println(img);
+            }
+            /*int count;
             if (dType.equals("3")) count = 3;
             else count = 23;
             int i = 0;
@@ -43,10 +52,13 @@ public class ExecuteServlet extends HttpServlet{
             for (String str : data.split(","))
                 inst[i++] = Double.parseDouble(str);
             if (i == (count)) System.out.println("Input Read Successfully: i: " + i);
-            for(double d:inst)System.out.println(d);
-
+            for(double d:inst)System.out.println(d);*/
+         }catch(Exception e)
+       {
+          e.printStackTrace();
+       }
             //Creating New Instance here
-            Instance instance = new DenseInstance(inst);
+            /*Instance instance = new DenseInstance(inst);
             String doc="<!DOCTYPE html><html lang=\"en\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1.0\"/><title>Machine Learning for Medical Data</title><!-- CSS  --><link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\"><link href=\"css\\materialize.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/><link href=\"css\\style.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/></head><body background=\"rsc/parallax.jpg\" onload=\"test();\"><div class=\"navbar-fixed\"><nav class=\"blue darken-1\" role=\"navigation\"><div class=\"nav-wrapper container\"><a id=\"logo-container\" href=\"index.html\" class=\"brand-logo\"><i class=\"material-icons \" style=\"font-size: 30px\">polymer</i></a></div></nav></div><div class=\"container white-text text-darken-1\"><div class=\"section\"><div class=\"row hoverable\"><h4 class=\"center light\">";
             //Creating Document
             out.println(doc);
@@ -138,7 +150,7 @@ public class ExecuteServlet extends HttpServlet{
 
            out.println("</h4>");
 
-        }catch(Exception e){System.out.println("Caught Exception!");e.printStackTrace();}
+        }catch(Exception e){System.out.println("Caught Exception!");e.printStackTrace();}*/
 
     }
 
