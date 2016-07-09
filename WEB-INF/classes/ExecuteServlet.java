b4e5c357217df8ebbf249878355ebf5b8a12592f
ShikharKGP <shikharsrivastav@gmail.com>
2016-07-09 19:39:35 +0530

    Javascript Alterations made

--- WEB-INF/classes/ExecuteServlet.java
+++ WEB-INF/classes/ExecuteServlet.java
@@ -6,16 +6,18 @@ import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import org.opencv.core.Core.*;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
-
 import javax.servlet.http.HttpServlet;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,7 +28,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ExecuteServlet extends HttpServlet {
 
-  static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
 
@@ -36,39 +38,56 @@ public class ExecuteServlet extends HttpServlet {
 
             String mType = request.getParameter("mType");
             System.out.println("mType: " + mType);
+            String[] lister=new String[]{"Nearest Mean Classifier","K Nearest Neigbors","MultiLayer Perceptron","AutoEncoders","Linear SVM","Self Optimizing SVM","Random Forest","Random Tree"};
+            String modelName;
+            switch(mType)
+            {
+              case "mlp": modelName=lister[2];break;
+              case "sosvm": modelName=lister[5];break;
+              case "nmc": modelName=lister[0];break;
+              case "knn": modelName=lister[1];break;
+              case "anc": modelName=lister[3];break;
+              case "svm": modelName=lister[4];break;
+              case "rff": modelName=lister[6];break;
+              case "rnt": modelName=lister[7];break;
+              default: //Materialize.toast("Wrong Model passed",6000);
+                        modelName=null;
+            }
             String imgName = request.getParameter("imgName");
             System.out.println("imgName: " + imgName);
             String[] imgNames = imgName.split("\\|\\|");
             System.out.println("Image(s) selected: ");
             String pathname, threshFilename, outputFilename;
             PrintWriter out=response.getWriter();
-            String docstart="<!DOCTYPE html><html lang=\"en\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1.0\"/><meta name=\"theme-color\" content=\"#5e42a6\"><title>ML for BMI</title><link rel=\"icon\" type=\"icon/ico\" href=\"rsc/icon.ico\"/><link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\"><link href=\"css\\materialize.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/><link href=\"css\\style.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/><link href=\"css\\dropify.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/><link href=\"css\\imgcenter.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/></head><body><div class=\"navbar-fixed\"><nav class=\"blue darken-3\" role=\"navigation\"><div class=\"row\"><div class=\"nav-wrapper container col s12\"><div class=\"nav-wrapper container col s12\"><a id=\"logo-container\" href=\"index.html\" class=\"brand-logo\"><i class=\"material-icons \" style=\"font-size: 35px\">polymer</i></a></div></div></div></nav></div><div class=\"parallax-container valign-wrapper hero \"><div class=\"container\"><div class=\"row center\"><h5 class=\"header col s12 light\"><br><br>Classification Result: <br><br><br></h5></div>";
-            String docend="<div class=\"parallax\"><img src=\"rsc/lawn.jpg\" alt=\"rsc/lawn.jpg\"></div></div></div><div class=\"section z-depth-5 white  darken-3\"><div class=\"container\"><div class=\"row\"><div class=\"col s12 m12\"><div class=\"icon-block\"><h5 class=\"center light\">ML for BMI</h5><h2 class=\"center light-blue-text\" ><a href=\"index.html\" ><i class=\"material-icons indigo-text\" style=\"font-size: 90px\">polymer</i></a></h2><h5 class=\"light\" align='center'>Using <b>Machine Learning</b> to solve Bio-Medical Imaging problems<br></h5></div></div></div></div></div><div class=\"parallax-container valign-wrapper\"><div class=\"container\"><h5 class=\"light\" align=\"center\"><br><br>Close this <b>page</b> to try other models </h5></div><div class=\"parallax\"><img src=\"rsc/lawn.jpg\" alt=\"Unsplashed background img 2\"></div></div><footer class=\"page-footer red darken-1\"><div class=\"container\"><div class=\"row\"><div class=\"col l6 s12 offset-l8\"><h5 class=\"white-text\">Machine Learning for Bio Medical Imaging</h5><p class=\"grey-text text-lighten-4\">Using Machine Learning models to solve problems in BioMedical Imaging</p></div></div></div><div class=\"footer-copyright\"><div class=\"container\">Made by <a class=\"red-text text-lighten-3\" href=\"#!\">Shikhar Srivastava</a> @IITKGP Summer2016</div></div></footer></body><script src=\"js/jquery-2.2.2.min.js\"></script><script src=\"js/materialize.js\"></script><script src=\"js/init.js\"></script><script src=\"js/dropify.js\"></script><script>$(document).ready(function() {$('.parallax').parallax();});</script><script>!function(n,r){\"function\"==typeof define&&define.amd?define(r):\"object\"==typeof exports?module.exports=r():n.transformicons=r()}(this||window,function(){\"use strict\";var n={},r=\"tcon-transform\",t={transform:[\"click\"],revert:[\"click\"]},e=function(n){return\"string\"==typeof n?Array.prototype.slice.call(document.querySelectorAll(n)):\"undefined\"==typeof n||n instanceof Array?n:[n]},o=function(n){return\"string\"==typeof n?n.toLowerCase().split(\" \"):n},f=function(n,r,f){var c=(f?\"remove\":\"add\")+\"EventListener\",u=e(n),s=u.length,a={};for(var l in t)a[l]=r&&r[l]?o(r[l]):t[l];for(;s--;)for(var d in a)for(var v=a[d].length;v--;)u[s][c](a[d][v],i)},i=function(r){n.toggle(r.currentTarget)};return n.add=function(r,t){return f(r,t),n},n.remove=function(r,t){return f(r,t,!0),n},n.transform=function(t){return e(t).forEach(function(n){n.classList.add(r)}),n},n.revert=function(t){return e(t).forEach(function(n){n.classList.remove(r)}),n},n.toggle=function(t){return e(t).forEach(function(t){n[t.classList.contains(r)?\"revert\":\"transform\"](t)}),n},n});</script></html>";
+            String docstart="<!DOCTYPE html><html lang=\"en\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1.0\"/><meta name=\"theme-color\" content=\"#5e42a6\"><title>ML for BMI</title><link rel=\"icon\" type=\"icon/ico\" href=\"rsc/icon.ico\"/><link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\"><link href=\"css\\materialize.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/><link href=\"css\\style.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/><link href=\"css\\dropify.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/><link href=\"css\\imgcenter.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/></head><body><div class=\"navbar-fixed\"><nav class=\"blue darken-3\" role=\"navigation\"><div class=\"row\"><div class=\"nav-wrapper container col s12\"><div class=\"nav-wrapper container col s12\"><a id=\"logo-container\" href=\"index.html\" class=\"brand-logo\"><i class=\"material-icons \" style=\"font-size: 35px\">polymer</i></a></div></div></div></nav></div><div class=\"parallax-container valign-wrapper hero \"><div class=\"container\"><div class=\"row center\"><h5 class=\"header col s12 light\"><br><br>Classification Result: "+modelName+" <br><br><br></h5></div>";
+            String docend="<div class=\"parallax\"><img src=\"rsc/lawn.jpg\" alt=\"rsc/lawn.jpg\"></div></div></div><div class=\"section z-depth-5 white  darken-3\"><div class=\"container\"><div class=\"row\"><div class=\"col s12 m12\"><div class=\"icon-block\"><h5 class=\"center light\">ML for BMI</h5><h2 class=\"center light-blue-text\" ><a href=\"index.html\" ><i class=\"material-icons indigo-text\" style=\"font-size: 90px\">polymer</i></a></h2><h5 class=\"light\" align='center'>Using <b>Machine Learning</b> to solve Bio-Medical Imaging problems<br></h5></div></div></div></div></div><div class=\"parallax-container valign-wrapper\"><div class=\"container\"><h5 class=\"light\" align=\"center\"><br><br>Close this <b>page</b> to try other models </h5></div><div class=\"parallax\"><img src=\"rsc/lawn.jpg\" alt=\"Unsplashed background img 2\"></div></div><footer class=\"page-footer red darken-1\"><div class=\"container\"><div class=\"row\"><div class=\"col l6 s12 offset-l8\"><h5 class=\"white-text\">Machine Learning for Bio Medical Imaging</h5><p class=\"grey-text text-lighten-4\">Using Machine Learning models to solve problems in BioMedical Imaging</p></div></div></div><div class=\"footer-copyright\"><div class=\"container\">Made by <a class=\"red-text text-lighten-3\" href=\"#!\">BioMedical Imaging Informatics, SMST</a> @IITKGP 2016</div></div></footer></body><script src=\"js/jquery-2.2.2.min.js\"></script><script src=\"js/materialize.js\"></script><script src=\"js/init.js\"></script><script src=\"js/dropify.js\"></script><script>$(document).ready(function() {$('.parallax').parallax();});</script><script>!function(n,r){\"function\"==typeof define&&define.amd?define(r):\"object\"==typeof exports?module.exports=r():n.transformicons=r()}(this||window,function(){\"use strict\";var n={},r=\"tcon-transform\",t={transform:[\"click\"],revert:[\"click\"]},e=function(n){return\"string\"==typeof n?Array.prototype.slice.call(document.querySelectorAll(n)):\"undefined\"==typeof n||n instanceof Array?n:[n]},o=function(n){return\"string\"==typeof n?n.toLowerCase().split(\" \"):n},f=function(n,r,f){var c=(f?\"remove\":\"add\")+\"EventListener\",u=e(n),s=u.length,a={};for(var l in t)a[l]=r&&r[l]?o(r[l]):t[l];for(;s--;)for(var d in a)for(var v=a[d].length;v--;)u[s][c](a[d][v],i)},i=function(r){n.toggle(r.currentTarget)};return n.add=function(r,t){return f(r,t),n},n.remove=function(r,t){return f(r,t,!0),n},n.transform=function(t){return e(t).forEach(function(n){n.classList.add(r)}),n},n.revert=function(t){return e(t).forEach(function(n){n.classList.remove(r)}),n},n.toggle=function(t){return e(t).forEach(function(t){n[t.classList.contains(r)?\"revert\":\"transform\"](t)}),n},n});</script></html>";
             //Creating Document
-             String fullpath="C:/apache-tomcat-8.0.36/webapps/MachineLearningIITKGP/";
+            String fullpath="C:/apache-tomcat-8.0.36/webapps/MachineLearningIITKGP/";
             out.println(docstart);
             for (String name : imgNames) {
 
-               
+
                 pathname ="uploads/" + name;
                 System.out.println("Path name: "+pathname);
 
                 //~~~~~~~~~~~~~~~~Reading Uploaded Image~~~~~~~~~~~~
                 Mat inputImg = Highgui.imread(fullpath+pathname, Highgui.CV_LOAD_IMAGE_COLOR);
-                
+
                 System.out.println("Sample Pixel vals: "+inputImg.get(0,0)[0]);
 
                 // ~~~~~~~~~~~~~~~~~~~~~~Adaptive Thresholding~~~~~~~~~~~~~~~~~~~~~~
-                Mat thresholded = AdaptSegmentImg(inputImg);  
-                  System.out.println("Thresholded Image Created");
+                Mat thresholded = AdaptSegmentImg(inputImg);
+                System.out.println("Thresholded Image Created");
 
                 threshFilename = "output/" + name.substring(0, name.indexOf('.')) + "_thresh"+ name.substring(name.indexOf('.'));
                 Highgui.imwrite(fullpath+threshFilename, thresholded);
-                  System.out.println("Thresholded Image Saved. At: "+fullpath+threshFilename);
+                System.out.println("Thresholded Image Saved. At: "+fullpath+threshFilename);
                 outputFilename = "output/" + name.substring(0, name.indexOf('.')) + "_out_"+mType+ name.substring(name.indexOf('.'));
 
-                Mat outputImg = new Mat(inputImg.rows(), inputImg.cols(), inputImg.type());
+                Mat finalOutputImg = new Mat(2*inputImg.rows(), inputImg.cols(), inputImg.type());
                 System.out.println("Creating new outputImg");
+                Mat tempOutputImg= new Mat(inputImg.rows(),inputImg.cols(),inputImg.type());
+
                 if (mType.equals("nmc") || mType.equals("knn") || mType.equals("nvb") || mType.equals("svm") || mType.equals("sosvm") || mType.equals("rbfsvm") || mType.equals("rff") || mType.equals("rnt"))
                 {
                     System.out.println("Using JavaML Classifier....");
@@ -77,24 +96,31 @@ public class ExecuteServlet extends HttpServlet {
                         new FileInputStream("C:/apache-tomcat-8.0.36/webapps/MachineLearningIITKGP/models/" + mType + ".bin"));
                     ObjectInputStream ois = (ObjectInputStream) is;
                     model = (Classifier) ois.readObject();
-                       System.out.println("Loaded Classifier Model...JAVAML");
+                    System.out.println("Loaded Classifier Model...JAVAML");
                     Instance inst;
                     double arr[];
-                    for (int i = 0; i < inputImg.rows(); i++) {
-                        for (int j = 0; j < inputImg.cols(); j++) {
+
+                    for (int i =0; i < inputImg.rows(); i++) {
+                        for (int j =0; j < inputImg.cols(); j++) {
 
                             if (thresholded.get(i, j)[0] > 0) {
                                 arr = inputImg.get(i, j);
                                 inst = new DenseInstance(new double[]{arr[2], arr[1], arr[0]});
                                 int predicted = Integer.parseInt(model.classify(inst).toString());
                                 if (predicted == 1) {
-                                    outputImg.put(i, j, new double[]{255, 0, 0});
-                                } else outputImg.put(i, j, new double[]{0, 0, 255});
-                            } else outputImg.put(i, j, inputImg.get(i, j));
+                                    tempOutputImg.put(i, j, new double[]{255, 0, 0});
+                                } else tempOutputImg.put(i, j, new double[]{0, 0, 255});
+                            } else tempOutputImg.put(i, j, inputImg.get(i, j));
 
                         }
                     }
                     System.out.println("Output Image Created..");
+                    //finalOutputImg.rowRange(0,inputImg.rows()-1).colRange(0,inputImg.cols()-1)=inputImg;
+                    //finalOutputImg.rowRange(inputImg.rows(),2*inputImg.rows()-1).colRange(0,inputImg.cols()-1)=tempOutputImg;
+                    Core.vconcat(Arrays.asList(inputImg,tempOutputImg),finalOutputImg);
+
+                    System.out.println("Mapping to Final Output");
+
 
                 } else {
 
@@ -130,29 +156,33 @@ public class ExecuteServlet extends HttpServlet {
                                 }
                                 max_class++;
                                 if (max_class == 1) {
-                                    outputImg.put(i, j, new double[]{0, 0, 255});
-                                } else outputImg.put(i, j, new double[]{255,0,0});
+                                    tempOutputImg.put(i, j, new double[]{0, 0, 255});
+                                } else tempOutputImg.put(i, j, new double[]{255,0,0});
 
                             }
-                            else outputImg.put(i, j, inputImg.get(i, j));
+                            else tempOutputImg.put(i, j, inputImg.get(i, j));
 
 
                         }
                     }
-
+                    System.out.println("Output Image Created..");
+                    //finalOutputImg.rowRange(0,inputImg.rows()-1).colRange(0,inputImg.cols()-1)=inputImg;
+                    //finalOutputImg.rowRange(inputImg.rows(),2*inputImg.rows()-1).colRange(0,inputImg.cols()-1)=tempOutputImg;
+                    Core.vconcat(Arrays.asList(inputImg,tempOutputImg),finalOutputImg);
+                    System.out.println("Mapping to Final Output");
 
                 }
                 //~~~~~~~~~~~~~~~Writing Output Image~~~~~~~~~~~~~~~~~~~
-                Highgui.imwrite(fullpath+outputFilename, outputImg);
-               System.out.println("Output Image written to: "+fullpath+outputFilename);
-               
-               //~~~~~~~~~~~~~~~~Writing Image HTML Script to Pop-Up Page~~~~~~~~~~~~~~~~~~~~~~~~
-                out.println("<div class=\"row\" align=\"center\"><img class=\"display materialboxed\" width=\"400\" align=\"center\" style=\"float:right\" src=\""+pathname+"\"><img class=\"display materialboxed\" width=\"400\" align=\"center\" style=\"float:left\" src=\""+outputFilename+"\"></div>");
+                Highgui.imwrite(fullpath+outputFilename, finalOutputImg);
+                System.out.println("Output Image written to: "+fullpath+outputFilename);
+
+                //~~~~~~~~~~~~~~~~Writing Image HTML Script to Pop-Up Page~~~~~~~~~~~~~~~~~~~~~~~~
+                out.println("<div class=\"row\" align=\"center\"><img class=\"display materialboxed\" width=\"400\" align=\"center\" style=\"float:center\" src=\""+outputFilename+"\"></div>");
 
             }
-             
-                //~~~~~~~~~~~~~~~~~~~~~~~~~~~~Printing Pop-Up Page-HTML/JavaScript here~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                   out.println(docend);
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~Printing Pop-Up Page-HTML/JavaScript here~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            out.println(docend);
         }catch(Exception e)
         {
             e.printStackTrace();
@@ -165,16 +195,16 @@ public class ExecuteServlet extends HttpServlet {
 
 
     }
-        public Mat AdaptSegmentImg(Mat inputImg)
-        {
-            Mat result = new Mat();
-            ArrayList<Mat> rgb_planes = new ArrayList<>();
-            Core.split(inputImg, rgb_planes);
-            int win = inputImg.cols()/20;
-            win = 2 * ( (win/ 2 )) + 1;
-            Imgproc.adaptiveThreshold(rgb_planes.get(1), result, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, win , 1);
-            return result;
-        }
+    public Mat AdaptSegmentImg(Mat inputImg)
+    {
+        Mat result = new Mat();
+        ArrayList<Mat> rgb_planes = new ArrayList<>();
+        Core.split(inputImg, rgb_planes);
+        int win = inputImg.cols()/20;
+        win = 2 * ( (win/ 2 )) + 1;
+        Imgproc.adaptiveThreshold(rgb_planes.get(1), result, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, win , 1);
+        return result;
+    }
 
 
 }
--- js/coolstuff.js
+++ js/coolstuff.js
@@ -83,7 +83,7 @@ function find_file(name) {
         var appendStr="";
         for(var i=0;i<imgs.length;i++)
         {
-            appendStr+='<li><img src="uploads/'+imgs[i]+'"><div class="caption center-align"><h5 class="light black-text text-lighten-3 center"><br><br><br><br>Scroll down to select<br><b>ML Model</b><br><br><br></h5><div class="row center"><span class="tcon-indicator" aria-label="scroll" aria-hidden="true"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" class="tcon-svgchevron" viewBox="0 0 30 36"><path class="a3" d="M0,0l15,16L30,0"></path> <path class="a2"   d="M0,10l15,16l15-16"></path><path class="a1" d="M0,20l15,16l15-16"></path></svg></span></div></div></li>';
+            appendStr+='<li><img src="uploads/'+imgs[i]+'"><div class="caption center-align"><h5 class="light black-text text-lighten-3 center"><br><br><br><br>Scroll down to select<br><b>ML Model</b><br><br><br></h5><div class="row center"></div></div></li>';
         }
           return appendStr;
 
