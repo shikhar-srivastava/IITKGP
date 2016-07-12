8ab7dedc16f7450224492e182f6f41f551bd12af
Shikhar Srivastava <shikharsrivastav@gmail.com>
2016-07-12 23:00:49 +0530

    Orignal cache input for testing

--- js/init.js
+++ js/init.js
@@ -31,3 +31,20 @@ var getCookie = function(cname) {
     }
     return "";
 }
+
+var getAppend = function(cname,strs) {
+    var name = cname + "=";
+    var ca = strs.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) {
+          var len1 = name.length;
+          var len2 = c.length;
+          if(c.charAt(len1) == '"') len1++;
+          if(c.charAt(len2-1) == '"') len2--;
+          return c.substring(len1,len2);  //remove those pesky quotaions
+        }
+    }
+    return "";
+}
