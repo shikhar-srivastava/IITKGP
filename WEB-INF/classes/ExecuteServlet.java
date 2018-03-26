dea35efbc6522270005a9f86729784c92ef620ba
Shikhar Srivastava <shikharsrivastav@gmail.com>
2018-03-26 11:42:15 +0530

    Install ready
    1. Added batch files for install
    2. Added comprehensive Readme
    3. All Dependencies added

--- WEB-INF/classes/ExecuteServlet.java
+++ WEB-INF/classes/ExecuteServlet.java
@@ -53,6 +53,7 @@ public class ExecuteServlet extends HttpServlet {
               default: //Materialize.toast("Wrong Model passed",6000);
                         modelName=null;
             }
+            System.out.println("Model Requested: "+modelName);
             String imgName = request.getParameter("imgName");
             System.out.println("imgName: " + imgName);
             String[] imgNames = imgName.split("\\|\\|");
@@ -62,7 +63,10 @@ public class ExecuteServlet extends HttpServlet {
             String docstart="<!DOCTYPE html><html lang=\"en\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1.0\"/><meta name=\"theme-color\" content=\"#5e42a6\"><title>ML for BMI</title><link rel=\"icon\" type=\"icon/ico\" href=\"rsc/icon.ico\"/><link href=\"https://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\"><link href=\"css\\materialize.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/><link href=\"css\\style.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/><link href=\"css\\dropify.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/><link href=\"css\\imgcenter.css\" type=\"text/css\" rel=\"stylesheet\" media=\"screen,projection\"/></head><body><div class=\"navbar-fixed\"><nav class=\"blue darken-3\" role=\"navigation\"><div class=\"row\"><div class=\"nav-wrapper container col s12\"><div class=\"nav-wrapper container col s12\"><a id=\"logo-container\" href=\"index.html\" class=\"brand-logo\"><i class=\"material-icons \" style=\"font-size: 35px\">polymer</i></a></div></div></div></nav></div><div class=\"parallax-container valign-wrapper hero \"><div class=\"container\"><div class=\"row center\"><h5 class=\"header col s12 light\"><br><br>Classification Result: "+modelName+" <br><br><br></h5></div>";
             String docend="<div class=\"parallax\"><img src=\"rsc/lawn.jpg\" alt=\"rsc/lawn.jpg\"></div></div></div><div class=\"section z-depth-5 white  darken-3\"><div class=\"container\"><div class=\"row\"><div class=\"col s12 m12\"><div class=\"icon-block\"><h5 class=\"center light\">ML for BMI</h5><h2 class=\"center light-blue-text\"><a href=\"index.html\" ><i class=\"material-icons indigo-text\" style=\"font-size: 90px\">polymer</i></a></h2><h5 class=\"light\" align='center'>Using <b>Machine Learning</b> to solve Bio-Medical Imaging problems<br></h5></div></div></div></div></div><div class=\"parallax-container valign-wrapper\"><div class=\"container\"><h5 class=\"light\" align=\"center\"><br><br>Close this <b>page</b> to try other models </h5></div><div class=\"parallax\"><img src=\"rsc/lawn.jpg\" alt=\"Unsplashed background img 2\"></div></div><footer class=\"page-footer red darken-1\"><div class=\"container\"><div class=\"row center\"><div class=\"col s12\"><a id=\"logo-container\" href=\"index.html\" class=\"brand-logo\"><img src=\"rsc/logo_small.png\" width=\"100\"></a><h5 class=\"white-text\">Machine Learning for Bio Medical Imaging</h5><p class=\"grey-text text-lighten-4\">Using Machine Learning models to solve problems in BioMedical Imaging</p></div></div></div><div class=\"footer-copyright\"><div class=\"container\"><a class=\"red-text text-lighten-3\" href=\"#!\">BioMedical Imaging Informatics, SMST</a> @IITKGP 2016</div></div></footer></body><script src=\"js/jquery-2.2.2.min.js\"></script><script src=\"js/materialize.js\"></script><script src=\"js/init.js\"></script><script src=\"js/dropify.js\"></script><script>$(document).ready(function() {$('.parallax').parallax();});</script><script>!function(n,r){\"function\"==typeof define&&define.amd?define(r):\"object\"==typeof exports?module.exports=r():n.transformicons=r()}(this||window,function(){\"use strict\";var n={},r=\"tcon-transform\",t={transform:[\"click\"],revert:[\"click\"]},e=function(n){return\"string\"==typeof n?Array.prototype.slice.call(document.querySelectorAll(n)):\"undefined\"==typeof n||n instanceof Array?n:[n]},o=function(n){return\"string\"==typeof n?n.toLowerCase().split(\" \"):n},f=function(n,r,f){var c=(f?\"remove\":\"add\")+\"EventListener\",u=e(n),s=u.length,a={};for(var l in t)a[l]=r&&r[l]?o(r[l]):t[l];for(;s--;)for(var d in a)for(var v=a[d].length;v--;)u[s][c](a[d][v],i)},i=function(r){n.toggle(r.currentTarget)};return n.add=function(r,t){return f(r,t),n},n.remove=function(r,t){return f(r,t,!0),n},n.transform=function(t){return e(t).forEach(function(n){n.classList.add(r)}),n},n.revert=function(t){return e(t).forEach(function(n){n.classList.remove(r)}),n},n.toggle=function(t){return e(t).forEach(function(t){n[t.classList.contains(r)?\"revert\":\"transform\"](t)}),n},n});</script></html>";
             //Creating Document
-            String fullpath="C:/apache-tomcat-8.0.36/webapps/MachineLearningIITKGP/";
+            
+            // WebApp Abs. Path: "C:/apache-tomcat-8.0.36/webapps/MachineLearningIITKGP/"
+            
+            String fullpath=getServletContext().getRealPath("/");
             out.println(docstart);
             for (String name : imgNames) {
 
@@ -94,10 +98,10 @@ public class ExecuteServlet extends HttpServlet {
                     System.out.println("Using JavaML Classifier....");
                     Classifier model;
                     InputStream is = new ObjectInputStream(
-                        new FileInputStream("C:/apache-tomcat-8.0.36/webapps/MachineLearningIITKGP/models/" + mType + ".bin"));
+                        new FileInputStream(fullpath+"models/" + mType + ".bin"));
                     ObjectInputStream ois = (ObjectInputStream) is;
                     model = (Classifier) ois.readObject();
-                    System.out.println("Loaded Classifier Model...JAVAML");
+                    System.out.println("Loaded Classifier Model....");
                     Instance inst;
                     double arr[];
 
@@ -124,14 +128,13 @@ public class ExecuteServlet extends HttpServlet {
 
                     System.out.println("Mapping to Final Output");
 
-
                 } else {
 
                     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Reading DeepLearning4j Models here~~~~~~~~~~~~~~~~~~~~~
 
-                    System.out.println("Model selected is: "+mType);
-                    MultiLayerConfiguration confFromJson = MultiLayerConfiguration.fromJson(FileUtils.readFileToString(new File("C:/apache-tomcat-8.0.36/webapps/MachineLearningIITKGP/models/"+mType+".json")));
-                    DataInputStream dis = new DataInputStream(new FileInputStream("C:/apache-tomcat-8.0.36/webapps/MachineLearningIITKGP/models/"+mType+".bin"));
+                    System.out.println("Model selected is: "+modelName+". "+"("+mType+")");
+                    MultiLayerConfiguration confFromJson = MultiLayerConfiguration.fromJson(FileUtils.readFileToString(new File(fullpath+"models/"+mType+".json")));
+                    DataInputStream dis = new DataInputStream(new FileInputStream(fullpath+"models/"+mType+".bin"));
                     INDArray newParams = Nd4j.read(dis);
                     dis.close();
                     MultiLayerNetwork model = new MultiLayerNetwork(confFromJson);
@@ -141,6 +144,7 @@ public class ExecuteServlet extends HttpServlet {
 
                     System.out.println("Model Re-created from stored .json and .bin files");
 
+                    //~~~~~~~~~~~~~~~~~~~~~~~~Model Function on Image: Model interprets Image by classifying each pixel of the image~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                     INDArray predicted;
                     int max_prob,k,max_class;
                     for (int i = 0; i < inputImg.rows(); i++) {
@@ -179,6 +183,7 @@ public class ExecuteServlet extends HttpServlet {
                 //~~~~~~~~~~~~~~~Writing Output Image~~~~~~~~~~~~~~~~~~~
                 Highgui.imwrite(fullpath+outputFilename, finalOutputImg);
                 System.out.println("Output Image written to: "+fullpath+outputFilename);
+                System.out.println("~~~~~~~~MODEL EXECUTION COMPLETED~~~~~~~\n\n");
 
                 //~~~~~~~~~~~~~~~~Writing Image HTML Script to Pop-Up Page~~~~~~~~~~~~~~~~~~~~~~~~
                 out.println("<div class=\"row\" align=\"center\"><img class=\"display materialboxed\" width=\"400\" align=\"center\" style=\"float:center\" src=\""+outputFilename+"\"></div>");
